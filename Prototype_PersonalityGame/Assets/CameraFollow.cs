0084853bb04794e57d607500339d2d0fd7de52a6
Haydn Murray <haydn.murray1@yahoo.co.uk>
2018-05-28 13:32:04 +0100

    Ability to move camera with mouse keys (locked on player still)

--- Prototype_PersonalityGame/Assets/CameraFollow.cs
+++ Prototype_PersonalityGame/Assets/CameraFollow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,7 @@ public class CameraFollow : MonoBehaviour
 
     public Transform player;                // our player
     public float smoothSpeed = 1.5f;      // speed to smooth transitions instead of snapping to player
-    public Vector3 offset;                  // how far back on x, y, z we want to be from our player (otherwise we'll be inside our player)
+    public Vector3 offset;                 // how far back on x, y, z we want to be from our player (otherwise we'll be inside our player)
 
 
     // Fixed update moves after update() so the player would've
@@ -15,7 +16,41 @@ public class CameraFollow : MonoBehaviour
     private void FixedUpdate()
     {
         CheckForScrollWheel();
+        CheckForKeyboardMovement();
         FollowPlayer();
+        
+    }
+
+    private void CheckForKeyboardMovement()
+    {
+        // So the camera is spun around. Hack to get it to move correctly.
+        Vector3 forward = -Vector3.forward - Vector3.right;
+        Vector3 back    = -forward;
+        Vector3 right   = Vector3.forward + Vector3.left;
+        Vector3 left    = -right;
+
+        Vector3 desiredPos = transform.position;
+        switch (Input.inputString)
+        {
+            case "w":
+                desiredPos += forward;
+                break;
+            case "s":
+                desiredPos += back;
+                break;
+            case "a":
+                desiredPos += left;
+                break;
+            case "d":
+                desiredPos += right;
+                break;
+            default:
+                desiredPos = transform.position;
+                break;
+        }
+
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, 100 * Time.deltaTime);
+        transform.position = smoothedPos;           
     }
 
     private void FollowPlayer()
@@ -44,6 +79,7 @@ public class CameraFollow : MonoBehaviour
     private void SetOffset(Vector3 newOffset)
     {
         // We validate the new offset here so that we don't go to close or to far.
+        // TODO maybe replace with Mathf.Clamp to keep a value between a min/max
         if (newOffset.x > 1 && newOffset.x < 10)
         {
             offset = newOffset;
