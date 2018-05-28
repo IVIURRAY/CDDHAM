5c4c541e1094dd8dc88e470b2f28485f3080e155
Haydn Murray <haydn.murray1@yahoo.co.uk>
2018-05-28 11:00:22 +0100

    Ability to move player when clicking on the screen

---
+++ Prototype_PersonalityGame/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent agent;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Shoot a ray from the camera to where our mouse is pointing.
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If we actually hit something then lets move our player
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
